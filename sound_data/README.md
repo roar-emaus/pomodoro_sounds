322432197e7ce06c077d1d738188761b72509c5d
Roar Emaus <roar@emaus.no>
2021-04-09 09:15:34 +0200

    Adding readme and license part for downloading sound bites. added script for downloading potential sounds

---
+++ sound_data/README.md
@@ -0,0 +1,4 @@
+# Free Sounds Library
+
+## License
+All sound bites downloaded from [FreeSoundsLibrary](https://www.freesoundslibrary.com/)
---
+++ sound_data/sound_download.sh
@@ -0,0 +1,4 @@
+wget https://www.freesoundslibrary.com/wp-content/uploads/2018/02/birds-chirping-sound-effect.zip
+wget https://www.freesoundslibrary.com/wp-content/uploads/2018/01/thunder-wind-and-rain-sound-effect.zip
+wget https://www.freesoundslibrary.com/wp-content/uploads/2020/06/rain-storm-sounds.zip
+wget https://www.freesoundslibrary.com/wp-content/uploads/2020/05/rain-storm-in-the-city.zip
